d4f29a4d86f795579242674972d9d08ca296b129
genmbjp <genmbjp@gmail.com>
2026-08-06 08:27:19 +0700

    Update import path for useToast in Signup component

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { signUp } from '../lib/supabase'
-import { useToast } from '../App'
+import { useToast } from '../../contexts/ToastContext'
 
 export default function Signup() {
   const [fullName, setFullName] = useState('')
